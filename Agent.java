27b92c923b90863e45f28a163ae68842b0803775
Miller Adulu <info@milleradulu.co.ke>
2018-09-10 11:04:23 +0300

    Update agent

--- Agent.java
+++ Agent.java
@@ -7,6 +7,7 @@ class Agent {
 
     int noOfvertices; //number of vertices
     LinkedList<SystemState> mainGraph[]; // array of lists of vertices
+    static int path[]; //THhe solution path
 
     //constructor
 
@@ -77,17 +78,23 @@ class Agent {
         addEdge(newAgent, 12, returnState(12, 0, 0, 3, 3, 1, 14));
         addEdge(newAgent, 13, returnState(13, 0, 0, 3, 3, 1, 14));
 
-        //printGraph(newAgent);
+        // printGraph(newAgent);
 
-        System.out.println("Following is Depth First Traversal ,starting from vertex 0");
+        System.out.println("The following is Depth First Search, starting from vertex 0");
         newAgent.depthFirstSearch(newAgent, 0, 15);
+
+        System.out.println("The solution");
+              for(int i = 0; i < path.length; i++) {
+                System.out.print(i + " -> ");
+              }
+              System.out.println(" Goal");
 
     }
 
     void depthFirstSearch(Agent agent, int statrtVertex, int noOfvertices) {
         boolean visited[] = new boolean[noOfvertices];
+        path = new int[noOfvertices];
         depthFirstSearchProcess(agent, statrtVertex, visited);
-
     }
 
 
@@ -97,19 +104,39 @@ class Agent {
         System.out.print(startVertex + " \n");
 
         //check for adjacent nodes
+
         for (SystemState pCrawl : newAgent.mainGraph[startVertex]) {
+
             int nextNode = pCrawl.getNodeNumber();
+
             //check if the node is visited
-            if (!visited[nextNode]) {
+
+            if (!visited[nextNode] ) {
 
                 startVertex = nextNode;
-
+                
+                for(int i = 0; i < path.length; i++){
+                  
+                  path[i] = nextNode;
+                  
+                }
                 depthFirstSearchProcess(newAgent, startVertex, visited);
 
             }
 
-
         }
 
+    }
+
+    static boolean isSolution(SystemState state) {
+      if(
+        state.missionariesSideA == 0 &&
+        state.cannibalsSideA == 0 &&
+        state.missionariesSideB == 3 &&
+        state.cannibalsSideB == 3 &&
+        state.boatPosition == 1
+      ){ return true;}
+
+      return false;
     }
 }
